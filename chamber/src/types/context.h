80ee3a271b9cd253098921ff7016acf3c0daab65
Arnoldfiifi <139029515+Arnoldfiifi@users.noreply.github.com>
2026-01-21 21:22:47 -0600

    Add Context struct with relevant fields

--- chamber/src/types/context.h
+++ chamber/src/types/context.h
@@ -1,1 +1,12 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+
+struct Context {
+    std::string tenant_id;
+    std::string schema;
+    std::string request_payload;
+    std::unordered_map<std::string, std::string> metadata;
+    std::string decision;
+};
 
