ca4fd7fcbcf8210f04017f69a0cf380a07645ced
Arnoldfiifi <139029515+Arnoldfiifi@users.noreply.github.com>
2026-01-21 21:25:22 -0600

    Update tenant_registry.h

--- chamber/src/tenancy/tenant_registry.h
+++ chamber/src/tenancy/tenant_registry.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <string>
+
+struct TenantInfo {
+    std::string tenant_id;
+    std::string schema;
+};
+
+class TenantRegistry {
+public:
+    static TenantInfo resolve(const std::string& signed_key);
+};
 
