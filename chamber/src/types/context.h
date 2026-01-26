00293ad53ea656bcf6dddd6d5e7125a4a2e19586
Arnoldfiifi <139029515+Arnoldfiifi@users.noreply.github.com>
2026-01-26 16:39:40 -0600

    Update context.h

--- chamber/src/types/context.h
+++ chamber/src/types/context.h
@@ -3,10 +3,20 @@
 #include <unordered_map>
 
 struct Context {
+    // Identity
     std::string tenant_id;
     std::string schema;
+    std::string trace_id;
+    
+    // Request
+    std::string query;
     std::string request_payload;
+    
+    // Metadata
     std::unordered_map<std::string, std::string> metadata;
+    
+    // Decision
     std::string decision;
+    std::string route_to;
+    float confidence = 0.0f;
 };
-
